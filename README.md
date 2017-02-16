3efa25438e1785067bb0d23b3af6451c969d5fec
Adam Jun <adam@bluesky.pro>
2017-02-16 09:35:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# <https://docs.npmjs.com/>
+npm -v
+
+# <https://webpack.js.org/configuration/>
+npm install webpack --save-dev
+
+# <https://webpack.js.org/configuration/dev-server/>
+npm install webpack-dev-server --save-dev
+
+# <https://github.com/jantimon/html-webpack-plugin>
+npm install html-webpack-plugin --save-dev
+
+# <http://babeljs.io/docs/setup/>
+# <https://webpack.js.org/loaders/babel-loader/>
+npm install babel-loader babel-core babel-preset-es2015 babel-preset-react babel-preset-stage-0 --save-dev
+
+# <https://facebook.github.io/react/docs/installation.html>
+npm install react react-dom react-router --save
+
+# <http://redux.js.org/docs/basics/UsageWithReact.html>
+npm install redux react-redux --save
+
+
+
+npm update
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'main.bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            loader: 'babel-loader',
+            include: [
+                path.resolve(__dirname, 'src'),
+            ],
+            query: {
+                babelrc: false,
+                presets: [
+                    'es2015',
+                    'react',
+                    'stage-0'
+                ],
+            },
+        }, ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        })
+    ]
+};
+module.exports = config;
